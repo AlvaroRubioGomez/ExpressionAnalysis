64021d79a6ad34452f41aabf7bf535872bfa1a12
Alvaro <alvarorubiogomez90gmail.com>
2020-06-08 15:01:49 +0100

    all runs added, args added

---
+++ Results/readme.txt
@@ -0,0 +1,5 @@
+001
+-----------------
+Train - red
+Test - Blue
+Params - m Resnet18, LR 1e-05, BS 128, SS 10, G 0.1, No normalization
--- ft_vgg16_bn_v3.py
+++ ft_vgg16_bn_v3.py
@@ -9,8 +9,39 @@ from torchvision import datasets, models, transforms
 import sys
 from torch.utils.tensorboard import SummaryWriter
 
+import argparse
+
+#Arguments
+def parse_args():
+    parser = argparse.ArgumentParser()   
+
+    parser.add_argument("--img_root", type=str, 
+                        help="dataset directory", required=True)
+    parser.add_argument("--model", type=str, 
+                        help="Pretained model selection",
+                        choices=[
+                            "Resnet18",
+                            "vgg_16_bn"], default='Resnet18')    
+    parser.add_argument("-v", "--verbose", 
+                        help="increase output verbosity", action="store_true") 
+    parser.add_argument("--hyperparam", type=int, nargs=2, 
+                        help="Couple of integers to define batch size and number of epochs in this order", default=[128,25])
+    parser.add_argument("--lr", type=float,
+                        help="Learning rate", default=0.00001)
+    parser.add_argument("--lr_scheduler", type=float, nargs=3,
+                        help="Learning scheduler selected. Three float numbers to define type, step size and gamma in this order.\n Type:(0) None, (1)Step",
+                        default=[1,10,0.1])
+    parser.add_argument("--opt", type=str, 
+                        help="Optimizer selection",
+                        choices=[
+                            "Adam",
+                            "SGD"], default='Adam')
+
+    return parser.parse_args()
+
+    
 #Data loader
-def get_data(batch_size, img_root):
+def get_data(batch_size, img_root, verbose):
 
     #Define transformation that you wish to apply on image
     data_transforms = transforms.Compose([
@@ -27,7 +58,9 @@ def get_data(batch_size, img_root):
     # Create train and test splits
     # We will create a 80:20 % train:test split
     num_samples = len(image_datasets)
-    print(num_samples)
+    if(verbose):
+        print("number of samples:", num_samples)
+
     training_samples = int(num_samples * 0.8 + 1)
     test_samples = num_samples - training_samples
 
@@ -41,21 +74,32 @@ def get_data(batch_size, img_root):
 
 
 #Load model
-def load_model(num_classes):
+def load_model(num_classes, model_name, verbose):
 
-    #Load the vgg16 pretrained
-    #vgg_16_bn
-    #model = models.vgg16_bn(pretrained=True) 
-    #resnet16
-    model = models.resnet18(pretrained=True)   
+    if(model_name=='vgg_16_bn'):
+        #Load the vgg16 pretrained
+        model = models.vgg16_bn(pretrained=True) 
 
-    #Output layer
-    #vgg_16_bn
-    #num_ftrs = model.classifier[6].in_features        
-    #model.classifier[6] = nn.Linear(num_ftrs, num_classes) # modified output
-    #resnet16
-    num_ftrs = model.fc.in_features        
-    model.fc = nn.Linear(num_ftrs, num_classes) # modified output
+        #Output layer       
+        num_ftrs = model.classifier[6].in_features        
+        model.classifier[6] = nn.Linear(num_ftrs, num_classes) # modified output
+
+    elif(model_name=='Resnet18'):
+        #Load the Resnet18 pretrained
+        model = models.resnet18(pretrained=True)   
+
+        #Output layer 
+        num_ftrs = model.fc.in_features        
+        model.fc = nn.Linear(num_ftrs, num_classes) # modified output
+    
+    else:
+        print("No model selected")
+        sys.exit()
+    
+    if(verbose):
+        print("Model:")
+        print(model)
+        print("----------------------------------------------")
 
     return model
 
@@ -67,24 +111,26 @@ def get_cost_function():
 
 
 #Optimizer
-def get_optimizer(model, learning_rate, optimizer_type='Adam', show=False):
+def get_optimizer(model, learning_rate, optimizer_type, verbose):
 
     if(optimizer_type=='SGD'):
         optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
     elif(optimizer_type=='Adam'):
         optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
     else:
         print("No optimizer selected")
         sys.exit()
     
-    if(show):
-        print("optimmizer:", optimizer.state_dict)
+    if(verbose):
+        print("optimizer params:", optimizer.state_dict)
     
     return optimizer
 
 
 #Test (of a batch)
-def test(model, test_loader, cost_function, device, show=False):
+def test(model, test_loader, cost_function, device, verbose):
     n_samples = 0.
     cumulative_loss = 0.
     cumulative_accuracy = 0.
@@ -106,14 +152,13 @@ def test(model, test_loader, cost_function, device, show=False):
             n_samples+= images.shape[0]
             cumulative_loss += loss.item() # Note: the .item() is needed to extract scalars from tensors
             _, predicted = outputs.max(1)
-            cumulative_accuracy += predicted.eq(labels).sum().item()  
-            #print(batch_id)
+            cumulative_accuracy += predicted.eq(labels).sum().item()              
 
             #test
             del images, labels, outputs, loss
           
 
-    if(show):
+    if(verbose):
         print(f'test_loss:{cumulative_loss/n_samples}')
         print(f'test_accuracy:{cumulative_accuracy/n_samples*100}')
 
@@ -152,7 +197,7 @@ def batch_train(model, images, labels, optimizer, cost_function, device):   #Tra
 
 
 
-def get_grid_images(data_loader, writer, tittle, show=False):
+def get_grid_images(data_loader, writer, tittle, verbose):
     #Tensorboard grid images
     img_batch_obj = iter(data_loader)
     example_data, example_targets = next(img_batch_obj)
@@ -160,23 +205,22 @@ def get_grid_images(data_loader, writer, tittle, show=False):
     writer.add_image('test loader batch', img_grid)
     writer.close()
 
-    if(show):
+    if(verbose):
         print("Image shape:", example_data.size())
 
 
 
-def main(batch_size = 128, 
-        learning_rate = 0.00001, 
-        step_size = 10,
-        gamma = 0.1,
-        num_epochs = 25,
-        num_classes = 7,
-        img_root = None):
-    
+def main(args):  
+
+    #Arguments
+    batch_size, num_epochs = args.hyperparam    
+    learning_rate = args.lr
+    lr_scheduler_flag, step_size, gamma = args.lr_scheduler   
+    num_classes = 7
+    img_root = args.img_root      
         
     #Data loader
-    train_loader, test_loader = get_data(batch_size=batch_size, 
-                                       img_root=img_root)
+    train_loader, test_loader = get_data(batch_size=batch_size, img_root=img_root, verbose=args.verbose)
     
     #classes
     classes = ('Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral')
@@ -185,31 +229,32 @@ def main(batch_size = 128,
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     #Tensorboard configuration
-    comment = f'LR:{learning_rate},BS:{batch_size}, SS:{step_size}, G:{gamma}, No normalization, Resnet18, dataaugx3'
+    comment = f'm:{args.model}, LR:{learning_rate},BS:{batch_size}, e:{num_epochs}, SS:{step_size}, G:{gamma}'
     writer_train = SummaryWriter('runs/train/' + comment)
     writer_test = SummaryWriter('runs/test/' + comment)
 
     #Load model
-    model = load_model(num_classes)
+    model = load_model(num_classes, args.model, args.verbose)
 
     #Load model into GPU
     model = model.to(device)
 
     #Optimizer and cost function
     optimizer = get_optimizer(model=model, learning_rate=learning_rate, 
-                                optimizer_type='Adam', show=True)
+                                optimizer_type=args.opt, verbose = args.verbose)
     cost_function = get_cost_function()
 
     #scheduler
-    step_lr_scheduler = lr_scheduler.StepLR(optimizer=optimizer, step_size=step_size, gamma=gamma) #Every 'step_size' epochs, learning rate is multiply by gamma.
+    if (lr_scheduler_flag != 0):
+        step_lr_scheduler = lr_scheduler.StepLR(optimizer=optimizer, step_size=step_size, gamma=gamma) #Every 'step_size' epochs, learning rate is multiply by gamma.
 
     #Grid images to tensorboard
-    get_grid_images(data_loader=test_loader, writer=writer_test, tittle='test loader batch', show=True)      
+    get_grid_images(data_loader=test_loader, writer=writer_test, tittle='test loader batch', verbose = args.verbose)      
         
     #Before training
     print('Before training:')
-    train_loss, train_accuracy = test(model, train_loader, cost_function, device)
-    test_loss, test_accuracy = test(model, test_loader, cost_function, device)    
+    train_loss, train_accuracy = test(model, train_loader, cost_function, device, args.verbose)
+    test_loss, test_accuracy = test(model, test_loader, cost_function, device, args.verbose)    
 
     print('\t Training loss {:.5f}, Training accuracy {:.2f}'.format(train_loss, train_accuracy))
     print('\t Test loss {:.5f}, Test accuracy {:.2f}'.format(test_loss, test_accuracy))
@@ -233,9 +278,8 @@ def main(batch_size = 128,
             if (batch_id+1) % 100 == 0: #print each quarter
                 
                  #Batch test loss and accuracy
-                test_loss, test_acc = test(model=model, test_loader=test_loader, cost_function=cost_function, device=device)
-                #print("test done")
-
+                test_loss, test_acc = test(model=model, test_loader=test_loader, cost_function=cost_function, device=device, verbose=args.verbose)
+                
                 print (f'Epoch [{epoch+1}/{num_epochs}], Step [{batch_id+1}/{n_total_steps}], Train Loss: {train_loss:.4f}, Train Acc: {train_acc}, Test Loss: {test_loss:.4f}, Test Acc: {test_acc}')
 
                 #Write tensorboard logs               
@@ -254,8 +298,10 @@ def main(batch_size = 128,
             del train_loss, train_acc
         
         #Apply scheduler
-        step_lr_scheduler.step()
-        print("LR:", step_lr_scheduler.get_last_lr())
+        if (lr_scheduler_flag != 0):
+            step_lr_scheduler.step()
+            if(args.verbose):
+                print("LR:", step_lr_scheduler.get_last_lr())
 
     #Close writer
     writer_test.close()
@@ -263,8 +309,8 @@ def main(batch_size = 128,
 
 
     print('After training:')
-    train_loss, train_accuracy = test(model, train_loader, cost_function, device)
-    test_loss, test_accuracy = test(model, test_loader, cost_function, device)
+    train_loss, train_accuracy = test(model, train_loader, cost_function, device, args.verbose)
+    test_loss, test_accuracy = test(model, test_loader, cost_function, device, args.verbose)
 
     print('\t Training loss {:.5f}, Training accuracy {:.2f}'.format(train_loss, train_accuracy))
     print('\t Test loss {:.5f}, Test accuracy {:.2f}'.format(test_loss, test_accuracy))
@@ -275,9 +321,19 @@ def main(batch_size = 128,
 #-------------------------------------------------------------------
 
 if __name__ == '__main__':
-    dataset_path = '/data/agomez/FER2013'
+    #dataset_path = '/data/agomez/FER2013'
     #dataset_path = 'C:/Users/Alvaro/Desktop/FER2013/FER'
-    main(img_root=dataset_path)
+    args = parse_args()
+    if(args.verbose):
+        print("img_root:", args.img_root) 
+        print("model:", args.model)
+        print("Batch size:", args.hyperparam[0],"Epochs:", args.hyperparam[1])
+        print("Learning rate:", args.lr)
+        print("lr_Scheduler:", args.lr_scheduler[0], "Step size:", args.lr_scheduler[1],
+                "Gamma:", args.lr_scheduler[2])
+        print("optimizer:", args.opt)     
+        
+    main(args)
 
 
 
